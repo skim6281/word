29409e1d3a01f5bb0228a77ebb46eb0a35fe2312
skim6281 <skim6281@gmail.com>
2017-05-30 19:25:40 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+//require our dependencies
+var path = require('path')
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+module.exports = {
+    context: __dirname,
+    entry: './assets/js/index',
+
+    output: {
+        path: path.resolve('./assets/bundles/'),
+        filename: 'bundle.js',
+    },
+
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ],
+
+    module: {
+        loaders: [
+            {
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+              query: {
+                presets: ['es2015','react']
+              }
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
